1dbe87c3fe6000a17e677f7bc48fabffbaa279d6
Simon Maribo <64478800+Simonmaribo@users.noreply.github.com>
2022-11-15 20:21:38 +0000

    Final Finish with next/prev buttons

--- components/DraggableContainer.vue
+++ components/DraggableContainer.vue
@@ -1,69 +1,88 @@
 <template>
-  <div
-    class="slider"
-    ref="slider"
-    @mousedown="mouseDown"
-    @mouseenter="mouseEnter"
-    @mouseleave="mouseLeave"
-    @mousemove.prevent="mouseMove"
-    @mouseup="mouseUp"
-  >
-    <slot />
-  </div>
+    <div>
+        <button @click.prevent="previous" :disabled="reachedScrollLimit == 1">Forrige</button>
+        <button @click.prevent="next" :disabled="reachedScrollLimit == 2">Næste</button>
+
+        <div class="slider" ref="slider" 
+            @mousedown="start" 
+            @mouseenter="stop" 
+            @mousemove.prevent="scroll"
+            @mouseleave="stop"
+            @mouseup="stop"
+            @scroll="checkIfReachedScrollLimit"
+        >
+            <slot/>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "DraggableContainer",
-  data() {
-    return {
-      startX: 0,
-      scrollLeft: 0,
-      isPressed: false,
-    };
-  },
-  methods: {
-    mouseDown(e) {
-      this.isPressed = true;
-      this.startX = e.pageX;
-      this.scrollLeft = this.$refs.slider.scrollLeft;
-      this.$refs.slider.style.cursor = "grabbing";
+    name: 'DraggableContainer',
+    data() {
+        return {
+            startX: 0, // X-position når drag startes
+            scrollLeft: 0, // Hvor meget der er scrollet
+            isPressed: false, // Om drag er startet eller ej (mousedown/mouseup)
+            reachedScrollLimit: 1 // 0 = no limit, 1 = left limit, 2 = right limit
+        }
     },
-    mouseEnter(e) {
-      this.$refs.slider.style.cursor = "grab";
-    },
-    mouseLeave(e) {
-      this.isPressed = false;
-      this.$refs.slider.style.cursor = "grab";
-    },
-    mouseUp(e) {
-      this.isPressed = false;
-      this.$refs.slider.style.cursor = "grab";
-    },
+    methods: {
+        start(e) {
+            this.isPressed = true
+            this.startX = e.pageX;
+            this.scrollLeft = this.$refs.slider.scrollLeft;
+            this.$refs.slider.style.cursor = 'grabbing';
+        },
+        stop(e) {
+            this.isPressed = false;
+            this.$refs.slider.style.cursor = 'grab';
+        },
 
-    mouseMove(e) {
-      if (this.isPressed) {
-        const distanceToStart = e.pageX - this.startX;
-        console.log(distanceToStart);
+        scroll(e) {
+            if(this.isPressed){
+                // Scroll containeren med den afstand som den har haft siden scroll-start.
+                this.scrollTo(this.scrollLeft - (e.pageX - this.startX), 'instant')
+            }
+        },
+        next() {
+            this.scrollTo(this.$refs.slider.scrollLeft + 420, 'smooth');
+        },
+        previous() {
+            this.scrollTo(this.$refs.slider.scrollLeft - 420, 'smooth');
+        },
+        scrollTo(x, behavior) {
+            this.$refs.slider.scrollTo({
+                left: x,
+                behavior: behavior || 'instant'
+            });
+            this.checkIfReachedScrollLimit();
+        },
+        checkIfReachedScrollLimit() {
 
-        this.$refs.slider.scrollTo({
-          left: this.scrollLeft - distanceToStart,
-          // distanceToStart falder (negativ) når du trækker til højre
-          // distanceToStart stiger (positiv) når du trækker til venstre
+            // Hvis der ikke er blevet scrollet (dragget)
+            if(this.$refs.slider.scrollLeft == 0) 
+                this.reachedScrollLimit = 1;
 
-          behavior: "instant",
-        });
-      }
-    },
-  },
-};
+            // Scroll Width = bredden af  scroll-containeren med overflow
+            // Scroll Left = hvor meget der er scrollet
+            // Client width = bredden af scroll-containeren uden overflow
+            // hvis scrollleft + clientwidth er lig med scrollwidth, så er der ikke mere at scrolle til højre
+            else if(this.$refs.slider.scrollLeft + this.$refs.slider.clientWidth >= this.$refs.slider.scrollWidth) 
+                this.reachedScrollLimit = 2;
+
+            else this.reachedScrollLimit = 0;
+        }
+    }
+}
 </script>
+
 
 <style scoped>
 .slider {
-  overflow: scroll;
-  display: flex;
-  gap: 10px;
-  cursor: grab;
+    overflow: hidden; /* scroll: to show scroll bars */
+    display: flex;
+    gap: 30px;
+    cursor: grab;
 }
 </style>
